b080454d85d6ffa78243e7f751913f901cbb6dfa
Devonte Whitely <dwhit85@lsu.edu>
2021-05-30 17:42:28 -0500

    Update server.js

--- server.js
+++ server.js
@@ -28,12 +28,29 @@ app.get("/api/hello", function (req, res) {
 app.get("/api/:date?", (req, res) => {
   let dateString = req.params.date;
   let passedInDate = new Date(dateString);
+
   let nullDate = new Date();
-  if (req.params.date = null) return res.json({"unix": nullDate.getTime(), "utc": nullDate.toUTCString()});
-  if (passedInDate == "Invalid Date") return res.json({"error" : "Invalid Date"});
+  let nullUnix = nullDate.getTime();
+  let nullUtc = nullDate.toUTCString();
+ 
+  if (req.params.date == null) {
+    return (
+      res.json({
+        "unix": nullUnix,
+        "utc": nullUtc
+      })
+    ); 
+  } else if (passedInDate == "Invalid Date") {
+    return (
+      res.json({
+        "error" : "Invalid Date"
+      })
+    ); 
+  } else {
   let unix = passedInDate.getTime();
   let utc = passedInDate.toUTCString();
-  return res.json({"unix": unix, "utc": utc});
+  return res.json({"unix": unix, "utc": utc});    
+  }
 });
 
 //Handle request to /api/1451001600000
